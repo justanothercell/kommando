00560c72f7d09489250d814dcab726312dc2db53
DragonFighter603 <75088766+DragonFighter603@users.noreply.github.com>
2024-06-14 16:44:03 +0200

    memory debug+check

--- bootstrap2/build/compile.sh
+++ bootstrap2/build/compile.sh
@@ -1,1 +1,1 @@
-gcc -g -rdynamic ../main.c ../token.c ../ast.c ../compiler.c ../parser.c ../lib/exit.c ../lib/gc.c ../lib/str.c ../lib/map.c -ldl -o compiler
+gcc -g -rdynamic ../main.c ../token.c ../ast.c ../compiler.c ../parser.c ../lib/exit.c ../lib/gc.c ../lib/str.c ../lib/map.c ../lib/debug.c -ldl -o compiler
