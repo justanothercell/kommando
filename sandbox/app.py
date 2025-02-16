6f434dc6c7998d00602426c724dd3db9b8ded1d3
dragoπ <75088766+justanothercell@users.noreply.github.com>
2025-02-16 19:44:14 +0100

    log

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, send_from_directory, request, redirect
+from flask import Flask, send_from_directory, request, redirect, abort
 import requests
+import logging
+
+logging.basicConfig(filename='log/latest.log', level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -13,6 +16,8 @@ def guide():
 
 @app.route('/docs/<path:item>')
 def guide_item(item):
+    if item.endswith('.html'):
+        print(f' [\x1b[1;33mEXEC\x1b[0m] \x1b[1m{request.remote_addr}\x1b[0m {item}')
     return send_from_directory('../docs/book/', item)
 
 @app.route('/editor.css')
@@ -21,8 +26,11 @@ def style():
 
 @app.route('/execute', methods = [ 'POST' ])
 def execute():
-    print(request.json)
-    r = requests.post('http://localhost:7878/execute', json=request.json)
+    print(f' [\x1b[1;33mEXEC\x1b[0m] \x1b[1m{request.remote_addr}\x1b[0m {request.json}')
+    try:
+        r = requests.post('http://localhost:7878/execute', json=request.json)
+    except Exception:
+        return abort(503)
     return r.json()
 
 if __name__ == '__main__':
