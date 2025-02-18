7aa5e3383d9f677ef19280adb8d9210dd200a913
dragoπ <75088766+justanothercell@users.noreply.github.com>
2025-02-18 17:41:11 +0100

    date

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory, request, redirect, abort
 import requests
 import logging
+import datetime
 
 logging.basicConfig(filename='log/latest.log', level=logging.INFO)
 
@@ -17,7 +18,7 @@ def guide():
 @app.route('/docs/<path:item>')
 def guide_item(item):
     if item.endswith('.html'):
-        print(f'[\x1b[1;34mDOCS\x1b[0m] \x1b[1m{request.remote_addr}\x1b[0m {item}')
+        print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} [\x1b[1;34mDOCS\x1b[0m] \x1b[1m{request.remote_addr}\x1b[0m {item}')
     return send_from_directory('../docs/book/', item)
 
 @app.route('/editor.css')
@@ -26,7 +27,8 @@ def style():
 
 @app.route('/execute', methods = [ 'POST' ])
 def execute():
-    print(f'[\x1b[1;33mEXEC\x1b[0m] \x1b[1m{request.remote_addr}\x1b[0m {request.json}')
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} [\x1b[1;33mEXEC\x1b[0m] \x1b[1m{request.remote_addr}\x1b[0m')
+    print(request.json)
     try:
         r = requests.post('http://localhost:7878/execute', json=request.json)
     except Exception:
